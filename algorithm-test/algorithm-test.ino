999026c61ab5fb304a582a765fdc36c373f04d28
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2022-03-06 20:10:25 +0500

    Add files via upload

--- algorithm-test/algorithm-test.ino
+++ algorithm-test/algorithm-test.ino
@@ -1,1 +1,39 @@
-just wait
+//--------------------------------------------------------------
+#define xPin A1
+#define yPin A2
+#define zPin A3
+//--------------------------------------------------------------
+// Take multiple samples to reduce noise
+const int samples = 10;
+//--------------------------------------------------------------
+
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+
+void loop() 
+{
+  //--------------------------------------------------------------
+  //Read raw values
+  int xRaw=0,yRaw=0,zRaw=0;
+  for(int i=0;i<samples;i++)
+  {
+    xRaw+=analogRead(xPin);
+    yRaw+=analogRead(yPin);
+    zRaw+=analogRead(zPin);
+  }
+  xRaw/=samples;
+  yRaw/=samples;
+  zRaw/=samples;
+  //--------------------------------------------------------------
+  Serial.print(xRaw);
+  Serial.print("\t");
+  Serial.print(yRaw);
+  Serial.print("\t");
+  Serial.print(zRaw);
+  Serial.println();
+  //--------------------------------------------------------------
+  delay(200);
+}
